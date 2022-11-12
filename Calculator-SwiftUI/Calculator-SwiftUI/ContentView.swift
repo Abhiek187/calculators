b76adf759820a24c6afd582754e47cf205b0d006
Abhishek Chaudhuri <abhishek.chaudhuri@prudential.com>
2022-11-12 14:10:44 -0500

    Fix Run Script warning & remove preview group

--- Calculator-SwiftUI/Calculator-SwiftUI/ContentView.swift
+++ Calculator-SwiftUI/Calculator-SwiftUI/ContentView.swift
@@ -438,10 +438,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView().preferredColorScheme(.light)
-            ContentView().preferredColorScheme(.dark)
-        }
+        ContentView()
     }
     // swiftlint:disable file_length
 }
