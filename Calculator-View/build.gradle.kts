c17cba23bb9450daf207cadbd46e1c492994b99f
Abhiek187 <achaudhuri2011@yahoo.com>
2023-04-21 20:40:56 -0400

    Migrate from Groovy to Kotlin

---
+++ Calculator-View/build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// TODO: remove once Gradle is >= 8.1
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
