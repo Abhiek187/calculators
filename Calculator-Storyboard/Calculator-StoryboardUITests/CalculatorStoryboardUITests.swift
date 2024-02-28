b66731aadba0972f64f89e355825306c88e9b063
Abhishek Chaudhuri <abhishek.chaudhuri@prudential.com>
2024-02-27 19:34:58 -0500

    Run each UI test once

--- Calculator-Storyboard/Calculator-StoryboardUITests/CalculatorStoryboardUITests.swift
+++ Calculator-Storyboard/Calculator-StoryboardUITests/CalculatorStoryboardUITests.swift
@@ -11,9 +11,9 @@ class CalculatorStoryboardUITests: XCTestCase {
     var app: XCUIApplication!
 
     // Run each UI test for every combination of orientation, localization, and appearance the app supports
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
