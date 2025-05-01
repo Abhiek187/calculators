f8fc371d03e33ce5ea2809c103b30fb81ba92e05
Abhiek187 <achaudhuri2011@yahoo.com>
2025-05-01 18:59:32 -0400

    Upgrade to Expo 53

--- Calculator-ReactNative/jest.config.js
+++ Calculator-ReactNative/jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   preset: "react-native",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"],
 };
